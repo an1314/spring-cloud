1061500f4b4a6369e0a46b269bfd8dade6b45370
liuchong <chong.liu@ebestmobile.com>
2020-03-12 16:44:55 +0800

    添加权限

---
+++ spring-cloud-oauth2-server/src/test/java/com/spcloud/springcloudoauth2server/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spcloud.springcloudoauth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
